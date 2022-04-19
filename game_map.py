4e83eaa8b9326a09fd1a6eb3a9ac7e2719a061f7
hwetsman <howard@tocdr.com>
2022-04-19 13:49:17 -0500

    Refactor: begin game map

---
+++ game_map.py
@@ -0,0 +1,14 @@
+game_map = {
+    1: ('Recovery!', 'rest'),
+    2: ('Choice', 'supply_choice_card'),
+    3: ('Move', 'supply_move_card'),
+    4: ('Rehab', 'rehab'),
+    5: ('Alumni Weekend', 'rest'),
+    6: ('choice_emblem', 'pick_choice_card'),
+    7: ('reaching_out', 'earn_chip'),
+    8: ('go_meeting', 'go_to_meeting'),
+    9: ('choice_emblem', 'pick_choice_card'),
+    10: ('miss_home_group', 'miss_home_group'),
+    11: ('you_are_ill', 'miss_next_two_home_groups'),
+    12: ('choice_emblem', 'pick_choice_card')
+}
