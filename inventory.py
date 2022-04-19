c352c618ff15d9a2a50e3b3bbf3bca2c7af285c2
hwetsman <howard@tocdr.com>
2022-04-19 15:59:41 -0500

    Refactor: add example function for saving update

--- inventory.py
+++ inventory.py
@@ -1,4 +1,20 @@
 
-inventory = {'income': {'meetings': 0, 'sponsees': 0, 'service': 0},
-             'costs': {'resentments': 0, 'distractions': 0},
+
+def Update_Savings(inventory):
+    income_dict = inventory['income']
+    income = income_dict['meetings']+income_dict['sponsees']+income_dict['service']
+    costs_dict = inventory['costs']
+    cost = costs_dict['resentments']+costs_dict['distractions']
+    savings = inventory['savings']-cost+income
+    print(savings)
+    return savings
+
+
+inventory = {'income': {'meetings': 1, 'sponsees': 2, 'service': 0},
+             'costs': {'resentments': 0, 'distractions': 1},
              'savings': 0}
+
+inventory['savings'] = Update_Savings(inventory)
+print(inventory)
+inventory['savings'] = Update_Savings(inventory)
+print(inventory)
