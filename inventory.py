4a9203d1cea5a563c683ae30bc9b2560b67472e5
hwetsman <howard@tocdr.com>
2022-04-19 16:20:34 -0500

    Refactor: add reset resentments and add resentments

--- inventory.py
+++ inventory.py
@@ -11,17 +11,34 @@ def Update_Savings(dict1):
     return dict1
 
 
-# def Add_Resentment(inventory):
+def Add_Resentment(inventory, quantity):
+    resentment = inventory['costs']['resentments']
+    resentment = resentment+quantity
+    inventory['costs']['resentments'] = max(resentment, 0)
+    return inventory
+
+
+def Reset_Resentment(inventory):
+    inventory['costs']['resentments'] = 0
+    return inventory
 
 
 inventory = {'income': {'meetings': 1, 'sponsees': 2, 'service': 0},
-             'costs': {'resentments': 0, 'distractions': 1},
+             'costs': {'resentments': 3, 'distractions': 1},
              'savings': 0}
 
 inventory = Update_Savings(inventory)
 print(inventory)
 print()
 inventory = Update_Savings(inventory)
+print(inventory)
+print()
+inventory = Add_Resentment(inventory, 2)
+print('Add Resentments')
+print(inventory)
+print()
+inventory = Reset_Resentment(inventory)
+print('Reset Resentments')
 print(inventory)
 print()
 print()
