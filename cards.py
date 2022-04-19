1948c0daf222554c4177c34fe1b8635b64fb13fe
hwetsman <howard@tocdr.com>
2022-04-19 15:44:44 -0500

    Code: first attempt to add cards

---
+++ cards.py
@@ -0,0 +1,14 @@
+cards = {
+    'no_choice': (8, {'See a trigger, lose 3 chips': None}),
+    'spouse_files': (2, {'Spouse files for divorce': ['1. Work 4th-5th step':cost]})
+}
+
+
+cards = {card_name: card_stem: {choice1: [text, get chips, lose chips, add costs, add income],
+                                choice2: [],
+                                choice3: [],
+                                choice4: []},
+         {no_choice: 'No Choice': {1: ['see a trigger, lose 3 chips', 0, 3, 0, 0]}},
+         {spouse_files: ''}
+
+         }
