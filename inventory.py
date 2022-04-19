be8ecbeb0b5ff1a2d32548764051b15094db6d6d
hwetsman <howard@tocdr.com>
2022-04-19 16:13:50 -0500

    Refactor: fix example code

--- inventory.py
+++ inventory.py
@@ -1,20 +1,27 @@
 
 
-def Update_Savings(inventory):
-    income_dict = inventory['income']
+def Update_Savings(dict1):
+    savings = dict1['savings']
+    income_dict = dict1['income']
     income = income_dict['meetings']+income_dict['sponsees']+income_dict['service']
-    costs_dict = inventory['costs']
+    costs_dict = dict1['costs']
     cost = costs_dict['resentments']+costs_dict['distractions']
-    savings = inventory['savings']-cost+income
-    print(savings)
-    return savings
+    total = savings-cost+income
+    dict1['savings'] = total
+    return dict1
+
+
+# def Add_Resentment(inventory):
 
 
 inventory = {'income': {'meetings': 1, 'sponsees': 2, 'service': 0},
              'costs': {'resentments': 0, 'distractions': 1},
              'savings': 0}
 
-inventory['savings'] = Update_Savings(inventory)
+inventory = Update_Savings(inventory)
 print(inventory)
-inventory['savings'] = Update_Savings(inventory)
+print()
+inventory = Update_Savings(inventory)
 print(inventory)
+print()
+print()
