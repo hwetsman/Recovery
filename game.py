3bd1d1f92ab41867cd7efda53d04c863a039d305
hwetsman <howard@tocdr.com>
2022-04-19 19:24:16 -0500

    Refactor: add randome roll

---
+++ game.py
@@ -0,0 +1,14 @@
+from inventory import Add_Cost, Reset_Cost, Update_Savings, inventory, Add_Income, Reset_Income
+from random import randint
+
+# inventory = {'income': {'meetings': 4, 'sponsees': 2, 'service': 0},
+#              'costs': {'resentments': 1, 'distractions': 1},
+#              'savings': 0}
+
+print()
+print(inventory)
+inventory = Add_Cost(inventory, 'resentments', 2)
+print(inventory)
+print()
+for i in range(10):
+    print(randint(1, 6))
