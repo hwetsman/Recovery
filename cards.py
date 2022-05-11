e411dda377b94ea19e928f640d03a5373b3c627c
hwetsman <howard@tocdr.com>
2022-05-11 14:59:17 -0500

    Code: create pick-a-card function

--- cards.py
+++ cards.py
@@ -1,22 +1,25 @@
 import pandas as pd
-import numpy
+import numpy as np
 
 file = 'cards.csv'
 
 df = pd.read_csv(file)
-print(df)
+# print(df)
 
-cards = list(set(df.card_name.tolist()))
-print(cards)
-deck = ['bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day',
-        'fired', 'fired', 'fired',
-        'divorce', 'divorce',
-        'birthday', 'birthday', 'birthday',
-        'less_than_10', 'less_than_10', 'less_than_10',
-        'first_step', 'first_step', 'first_step',
-        'bigshot', 'bigshot', 'bigshot',
-        'old_lover', 'old_lover', 'old_lover',
-        'trigger', 'trigger', 'trigger', 'trigger', 'trigger', 'trigger', 'trigger', 'trigger',
-        'opioid', 'opioid', 'opioid',
-        'bonus', 'bonus', 'bonus',
-        'fired', 'fired', 'fired', 'fired']
+deck = ['bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day',
+        'bad_day', 'bad_day', 'fired', 'fired', 'fired', 'divorce', 'divorce',
+        'birthday', 'birthday', 'birthday', 'less_than_10', 'less_than_10',
+        'less_than_10', 'first_step', 'first_step', 'first_step', 'bigshot',
+        'bigshot', 'bigshot', 'old_lover', 'old_lover', 'old_lover', 'opioid',
+        'opioid', 'opioid', 'trigger', 'trigger', 'trigger', 'trigger',
+        'trigger', 'trigger', 'trigger', 'trigger', 'bonus', 'bonus', 'bonus',
+        'fight', 'fight', 'fight', 'fight']
+
+
+def Pick_A_Card():
+    card = np.random.choice(deck)
+    return card
+
+
+card = Pick_A_Card()
+print(card)
