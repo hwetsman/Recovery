dc38350a9daaf8cd4edf15d820e8f3c29bf80294
hwetsman <howard@tocdr.com>
2022-05-11 14:42:32 -0500

    Code: create deck

--- cards.py
+++ cards.py
@@ -4,3 +4,18 @@ file = 'cards.csv'
 
 df = pd.read_csv(file)
 print(df)
+
+cards = list(set(df.card_name.tolist()))
+print(cards)
+deck = ['bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day', 'bad_day',
+        'fired', 'fired', 'fired',
+        'divorce', 'divorce',
+        'birthday', 'birthday', 'birthday',
+        'less_than_10', 'less_than_10', 'less_than_10',
+        'first_step', 'first_step', 'first_step',
+        'bigshot', 'bigshot', 'bigshot',
+        'old_lover', 'old_lover', 'old_lover',
+        'trigger', 'trigger', 'trigger', 'trigger', 'trigger', 'trigger', 'trigger', 'trigger',
+        'opioid', 'opioid', 'opioid',
+        'bonus', 'bonus', 'bonus',
+        'fired', 'fired', 'fired', 'fired']
