e4cd474eca65e444078bab0c4dcf48fb6d083aaf
hwetsman <howard@tocdr.com>
2022-05-11 12:36:02 -0500

    Refactor: have cards.py read csv

--- cards.py
+++ cards.py
@@ -1,21 +1,6 @@
-cards = {
-    'no_choice': (8, {'See a trigger, lose 3 chips': None}),
-    'spouse_files': (2, {'Spouse files for divorce': ['1. Work 4th-5th step':cost]})
-}
+import pandas as pd
 
+file = 'cards.csv'
 
-cards = {card_name: card_stem: {choice1: [text, {chips: ('add', -2)}, resentments: ('reset', 0),
-                                          distractions: (None, 0), sponsees: (None, 0),
-                                          service: (None, 0), meetings: (None, 0)],
-                                choice2: [text, {chips: ('add', -2)}, resentments: ('reset', 0),
-                                          distractions: (None, 0), sponsees: (None, 0),
-                                          service: (None, 0), meetings: (None, 0)],
-                                choice3: [text, {chips: ('add', -2)}, resentments: ('reset', 0),
-                                          distractions: (None, 0), sponsees: (None, 0),
-                                          service: (None, 0), meetings: (None, 0)],
-                                choice4: [text, {chips: ('add', -2)}, resentments: ('reset', 0),
-                                          distractions: (None, 0), sponsees: (None, 0),
-                                          service: (None, 0), meetings: (None, 0)]},
-
-
-         }
+df = pd.read_csv(file)
+print(df)
