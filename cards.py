b5144b20b2c7719a3aa8854226d135d4ce11aebb
hwetsman <howard@tocdr.com>
2022-05-13 09:52:26 -0500

    Refactor: create funcs for each class of card

--- cards.py
+++ cards.py
@@ -21,41 +21,53 @@ def Pick_A_Card():
     return card
 
 
+def First_Step_Card(card):
+    output['text'] = "Keep this card in case you need it but it is the next person's turn"
+    return output
+
+
+def No_Choice_Card(card):
+    output['text'] = "This is a no choice card"
+    temp = temp.loc[:, (temp != 0).any(axis=0)]
+    temp = temp.loc[:, (temp != 'None').any(axis=0)]
+    temp = temp.drop('choice', axis=1)
+    for col in temp.columns:
+        output[col] = temp.loc[0, col]
+    return output
+
+
+def Choice_Card(card):
+    output['text'] = "We all have choices"
+    temp = temp.loc[:, (temp != 0).any(axis=0)]
+    temp = temp.loc[:, (temp != 'None').any(axis=0)]
+    cols = [x for x in temp.columns if x not in ['card_name', 'card_stem']]
+    print(temp)
+    print(cols)
+
+    print('Your choices are:')
+    for i in range(temp.shape[0]):
+        print(f"{i}. {temp.loc[i,'choice']}")
+
+    for col in temp.columns:
+        output[col] = temp.loc[0, col]
+    print(output)
+    return output
+
+
 def Parse_Card(df, card):
     output = {}
     print()
     print(card)
     if card == 'first_step':
-        output['text'] = "Keep this card in case you need it but it is the next person's turn"
-        return output
+        First_Step_Card(card)
     else:
         temp = df[df.card_name == card]
         temp.reset_index(inplace=True, drop=True)
         print(temp)
         if temp.shape[0] == 1:
-            output['text'] = "This is a no choice card"
-            temp = temp.loc[:, (temp != 0).any(axis=0)]
-            temp = temp.loc[:, (temp != 'None').any(axis=0)]
-            temp = temp.drop('choice', axis=1)
-            for col in temp.columns:
-                output[col] = temp.loc[0, col]
-            return output
+            No_Choice_Card(card)
         else:
-            output['text'] = "We all have choices"
-            temp = temp.loc[:, (temp != 0).any(axis=0)]
-            temp = temp.loc[:, (temp != 'None').any(axis=0)]
-            cols = [x for x in temp.columns if x not in ['card_name', 'card_stem']]
-            print(temp)
-            print(cols)
-
-            print('Your choices are:')
-            for i in range(temp.shape[0]):
-                print(f"{i}. {temp.loc[i,'choice']}")
-
-            for col in temp.columns:
-                output[col] = temp.loc[0, col]
-            print(output)
-            return output
+            Choice_Card(card)
 
 
 card = Pick_A_Card()
