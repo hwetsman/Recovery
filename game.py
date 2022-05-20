0c8e7e99bad41e350a68ff449afc59303c478774
hwetsman <howard@tocdr.com>
2022-05-20 12:50:01 -0500

    Code: import pandas

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 from inventory import Add_Cost, Reset_Cost, Update_Savings, inventory, Add_Income, Reset_Income
 from random import randint
 from game_map import spaces
+import pandas as pd
 
 # inventory = {'income': {'meetings': 4, 'sponsees': 2, 'service': 0},
 #              'costs': {'resentments': 1, 'distractions': 1},
